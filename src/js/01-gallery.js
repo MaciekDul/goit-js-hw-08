d11f8b80551ea67b2b913937b49fb2112466aac5
MaciekDul <maciej.dul@onet.pl>
2023-03-06 20:24:43 +0100

    a
    a

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,49 @@
 // Add imports above this line
-import { galleryItems } from './gallery-items';
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
+import { galleryItems } from './gallery-items.js';
 // Change code below this line
+
+console.log(galleryItems);
+console.log(galleryItems);
+const galleryContainer = document.querySelector('.gallery');
+const cardsMarkup = createColorCardsMarkup(galleryItems);
+galleryContainer.insertAdjacentHTML('beforeend', cardsMarkup);
+
+galleryContainer.addEventListener('click', onGalleryContainerClick);
+
+function createColorCardsMarkup(galleryItems) {
+  return galleryItems
+    .map(({ preview, original, description }) => {
+      return `
+      <a class="gallery__item" href="${original}">
+      <img class="gallery__image" src="${preview}"
+       alt="${description}" />
+    </a> `;
+    })
+    .join('');
+}
+
+var lightbox = new SimpleLightbox('.gallery a', {
+  captions: true,
+  captionsData: 'alt',
+  captionPosition: 'bottom',
+  captionDelay: 250,
+});
+console.log(lightbox);
+
+function onGalleryContainerClick(evt) {
+  evt.preventDefault();
+  const isColorSwatchEl = evt.target.classList.contains('gallery__image');
+  if (!isColorSwatchEl) {
+    return;
+  }
+
+  window.addEventListener('keydown', e => {
+    if (e.key === 'Escape') {
+      instance.close();
+    }
+  });
+}
 
 console.log(galleryItems);
--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,37 @@
+import throttle from 'lodash.throttle';
 
+const formElement = document.querySelector('.feedback-form');
+const textareaElement = document.querySelector('.feedback-form  textarea');
+const inputEl = document.querySelector('input');
+
+const STORAGE_KEY = 'feedback-form-state';
+
+formElement.addEventListener('submit', onFormSubmit);
+formElement.addEventListener('input', throttle(onTextareaInput, 500));
+
+populateTextarea();
+let formData = {};
+
+function onFormSubmit(evt) {
+  evt.preventDefault();
+  evt.currentTarget.reset();
+  localStorage.removeItem(STORAGE_KEY);
+}
+function onTextareaInput(evt) {
+  formData[evt.target.name] = evt.target.value;
+  const saveDataEl = JSON.stringify(formData);
+  localStorage.setItem(STORAGE_KEY, saveDataEl);
+}
+
+function populateTextarea() {
+  const savedMessage = localStorage.getItem(STORAGE_KEY);
+
+  if (savedMessage) {
+    const pasrsedSav = JSON.parse(savedMessage);
+    let formData = {};
+    formData = pasrsedSav;
+    textareaElement.value = pasrsedSav.message;
+    inputEl.value = pasrsedSav.email;
+    console.log(pasrsedSav);
+  }
+}
